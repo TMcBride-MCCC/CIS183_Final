af42b3823acdbf54579ccdfe434200fac020e252
TMcBride-MCCC <tmcbride2@my.monroeccc.edu>
2024-11-07 22:48:52 -0500

    MealPlan
    -HorizontalScrollView GUI made
    
    Next for MealPlan: Code a loop to go through a group of cards and have an onItemSelectedListener for highlighting the selected card.

--- app/src/main/java/com/example/cis183_final/MealPlan.java
+++ app/src/main/java/com/example/cis183_final/MealPlan.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,13 +22,31 @@ public class MealPlan extends AppCompatActivity
 {
     //Create JAVA variables
     TextView tv_j_mealPlan_day;
-    HorizontalScrollView hsv_j_mealPlan_breakfast;
-    HorizontalScrollView hsv_j_mealPlan_lunch;
-    HorizontalScrollView hsv_j_mealPlan_dinner;
     Button btn_j_mealPlan_submit;
     BottomNavigationView bnv_j_mealPlan_bottomNav;
-
     Intent camefrom;
+    private CardView selectedCard;
+    //BREAKFAST
+    HorizontalScrollView hsv_j_mealPlan_breakfast;
+    CardView cv_j_mealPlan_breakfast1;
+    CardView cv_j_mealPlan_breakfast2;
+    CardView cv_j_mealPlan_breakfast3;
+    CardView cv_j_mealPlan_breakfast4;
+    CardView cv_j_mealPlan_breakfast5;
+    //LUNCH
+    HorizontalScrollView hsv_j_mealPlan_lunch;
+    CardView cv_j_mealPlan_lunch1;
+    CardView cv_j_mealPlan_lunch2;
+    CardView cv_j_mealPlan_lunch3;
+    CardView cv_j_mealPlan_lunch4;
+    CardView cv_j_mealPlan_lunch5;
+    //DINNER
+    HorizontalScrollView hsv_j_mealPlan_dinner;
+    CardView cv_j_mealPlan_dinner1;
+    CardView cv_j_mealPlan_dinner2;
+    CardView cv_j_mealPlan_dinner3;
+    CardView cv_j_mealPlan_dinner4;
+    CardView cv_j_mealPlan_dinner5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -38,12 +57,30 @@ public class MealPlan extends AppCompatActivity
 
         //Connect JAVA variables to GUI variables
         tv_j_mealPlan_day = findViewById(R.id.tv_v_mealPlan_day);
-        hsv_j_mealPlan_breakfast = findViewById(R.id.hsv_v_mealPlan_breakfast);
-        hsv_j_mealPlan_lunch = findViewById(R.id.hsv_v_mealPlan_lunch);
-        hsv_j_mealPlan_dinner = findViewById(R.id.hsv_v_mealPlan_dinner);
         btn_j_mealPlan_submit = findViewById(R.id.btn_v_mealPlan_submit);
         bnv_j_mealPlan_bottomNav = findViewById(R.id.bnv_v_mealPlan_bottomNav);
-
+        //BREAKFAST
+        hsv_j_mealPlan_breakfast = findViewById(R.id.hsv_v_mealPlan_breakfast);
+        cv_j_mealPlan_breakfast1 = findViewById(R.id.cv_v_mealPlan_breakfast1);
+        cv_j_mealPlan_breakfast2 = findViewById(R.id.cv_v_mealPlan_breakfast2);
+        cv_j_mealPlan_breakfast3 = findViewById(R.id.cv_v_mealPlan_breakfast3);
+        cv_j_mealPlan_breakfast4 = findViewById(R.id.cv_v_mealPlan_breakfast4);
+        cv_j_mealPlan_breakfast5 = findViewById(R.id.cv_v_mealPlan_breakfast5);
+        //LUNCH
+        hsv_j_mealPlan_lunch = findViewById(R.id.hsv_v_mealPlan_lunch);
+        cv_j_mealPlan_lunch1 = findViewById(R.id.cv_v_mealPlan_lunch1);
+        cv_j_mealPlan_lunch2 = findViewById(R.id.cv_v_mealPlan_lunch2);
+        cv_j_mealPlan_lunch3 = findViewById(R.id.cv_v_mealPlan_lunch3);
+        cv_j_mealPlan_lunch4 = findViewById(R.id.cv_v_mealPlan_lunch4);
+        cv_j_mealPlan_lunch5 = findViewById(R.id.cv_v_mealPlan_lunch5);
+        //DINNER
+        hsv_j_mealPlan_dinner = findViewById(R.id.hsv_v_mealPlan_dinner);
+        cv_j_mealPlan_dinner1 = findViewById(R.id.cv_v_mealPlan_dinner1);
+        cv_j_mealPlan_dinner2 = findViewById(R.id.cv_v_mealPlan_dinner2);
+        cv_j_mealPlan_dinner3 = findViewById(R.id.cv_v_mealPlan_dinner3);
+        cv_j_mealPlan_dinner4 = findViewById(R.id.cv_v_mealPlan_dinner4);
+        cv_j_mealPlan_dinner5 = findViewById(R.id.cv_v_mealPlan_dinner5);
+        
         //Functions
         getDay();
         submitButtonOnClickListener();
