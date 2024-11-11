6a54f1b96386b50e5197ce71185ce4a88514b8eb
TMcBride-MCCC <tmcbride2@my.monroeccc.edu>
2024-11-11 10:26:52 -0500

    NewRecipeInstructions
    -GUI Created
    -Java variables made
    -Java & GUI variables connected

--- app/src/main/java/com/example/cis183_final/NewRecipeInstructions.java
+++ app/src/main/java/com/example/cis183_final/NewRecipeInstructions.java
@@ -1,6 +1,9 @@
 package com.example.cis183_final;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class NewRecipeInstructions extends AppCompatActivity
 {
+    //Create JAVA variables
+    TextView tv_j_newRecipeInstructions_recipeName;
+    EditText et_j_newRecipeInstructions_instructions;
+    Button btn_j_newRecipeInstructions_addInstructions;
+    Button btn_j_newRecipeInstructions_doNotAddInstructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,11 +25,12 @@ public class NewRecipeInstructions extends AppCompatActivity
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new_recipe_instructions);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
-        {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //Connect JAVA variables to GUI variables
+        tv_j_newRecipeInstructions_recipeName = findViewById(R.id.tv_v_newRecipeInstructions_recipeName);
+        et_j_newRecipeInstructions_instructions = findViewById(R.id.et_v_newRecipeInstructions_instructions);
+        btn_j_newRecipeInstructions_addInstructions = findViewById(R.id.btn_v_newRecipeInstructions_addInstructions);
+        btn_j_newRecipeInstructions_doNotAddInstructions = findViewById(R.id.btn_v_newRecipeInstructions_doNotAddInstructions);
+
     }
 }
