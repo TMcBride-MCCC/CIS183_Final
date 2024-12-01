bf1f11ac6389c68ef82eb51798a75d29b84c2399
TMcBride-MCCC <tmcbride2@my.monroeccc.edu>
2024-12-01 17:15:54 -0500

    Update MealPlan.java
    Deleted old variables

--- app/src/main/java/com/example/cis183_final/MealPlan.java
+++ app/src/main/java/com/example/cis183_final/MealPlan.java
@@ -28,41 +28,11 @@ public class MealPlan extends AppCompatActivity
     DatabaseHelper dbHelper;
     private CardView selectedCard;
     //BREAKFAST
-    HorizontalScrollView hsv_j_mealPlan_breakfast;
-    CardView cv_j_mealPlan_breakfast1;
-    TextView tv_j_mealPlan_breakfast1;
-    CardView cv_j_mealPlan_breakfast2;
-    TextView tv_j_mealPlan_breakfast2;
-    CardView cv_j_mealPlan_breakfast3;
-    TextView tv_j_mealPlan_breakfast3;
-    CardView cv_j_mealPlan_breakfast4;
-    TextView tv_j_mealPlan_breakfast4;
-    CardView cv_j_mealPlan_breakfast5;
-    TextView tv_j_mealPlan_breakfast5;
+
     //LUNCH
-    HorizontalScrollView hsv_j_mealPlan_lunch;
-    CardView cv_j_mealPlan_lunch1;
-    TextView tv_j_mealPlan_lunch1;
-    CardView cv_j_mealPlan_lunch2;
-    TextView tv_j_mealPlan_lunch2;
-    CardView cv_j_mealPlan_lunch3;
-    TextView tv_j_mealPlan_lunch3;
-    CardView cv_j_mealPlan_lunch4;
-    TextView tv_j_mealPlan_lunch4;
-    CardView cv_j_mealPlan_lunch5;
-    TextView tv_j_mealPlan_lunch5;
+
     //DINNER
-    HorizontalScrollView hsv_j_mealPlan_dinner;
-    CardView cv_j_mealPlan_dinner1;
-    TextView tv_j_mealPlan_dinner1;
-    CardView cv_j_mealPlan_dinner2;
-    TextView tv_j_mealPlan_dinner2;
-    CardView cv_j_mealPlan_dinner3;
-    TextView tv_j_mealPlan_dinner3;
-    CardView cv_j_mealPlan_dinner4;
-    TextView tv_j_mealPlan_dinner4;
-    CardView cv_j_mealPlan_dinner5;
-    TextView tv_j_mealPlan_dinner5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -76,41 +46,11 @@ public class MealPlan extends AppCompatActivity
         btn_j_mealPlan_submit = findViewById(R.id.btn_v_mealPlan_submit);
         bnv_j_mealPlan_bottomNav = findViewById(R.id.bnv_v_mealPlan_bottomNav);
         //BREAKFAST
-        hsv_j_mealPlan_breakfast = findViewById(R.id.hsv_v_mealPlan_breakfast);
-        cv_j_mealPlan_breakfast1 = findViewById(R.id.cv_v_mealPlan_breakfast1);
-        tv_j_mealPlan_breakfast1 = findViewById(R.id.tv_v_mealPlan_breakfast1);
-        cv_j_mealPlan_breakfast2 = findViewById(R.id.cv_v_mealPlan_breakfast2);
-        tv_j_mealPlan_breakfast2 = findViewById(R.id.tv_v_mealPlan_breakfast2);
-        cv_j_mealPlan_breakfast3 = findViewById(R.id.cv_v_mealPlan_breakfast3);
-        tv_j_mealPlan_breakfast3 = findViewById(R.id.tv_v_mealPlan_breakfast3);
-        cv_j_mealPlan_breakfast4 = findViewById(R.id.cv_v_mealPlan_breakfast4);
-        tv_j_mealPlan_breakfast4 = findViewById(R.id.tv_v_mealPlan_breakfast4);
-        cv_j_mealPlan_breakfast5 = findViewById(R.id.cv_v_mealPlan_breakfast5);
-        tv_j_mealPlan_breakfast5 = findViewById(R.id.tv_v_mealPlan_breakfast5);
+
         //LUNCH
-        hsv_j_mealPlan_lunch = findViewById(R.id.hsv_v_mealPlan_lunch);
-        cv_j_mealPlan_lunch1 = findViewById(R.id.cv_v_mealPlan_lunch1);
-        tv_j_mealPlan_lunch1 = findViewById(R.id.tv_v_mealPlan_lunch1);
-        cv_j_mealPlan_lunch2 = findViewById(R.id.cv_v_mealPlan_lunch2);
-        tv_j_mealPlan_lunch2 = findViewById(R.id.tv_v_mealPlan_lunch2);
-        cv_j_mealPlan_lunch3 = findViewById(R.id.cv_v_mealPlan_lunch3);
-        tv_j_mealPlan_lunch3 = findViewById(R.id.tv_v_mealPlan_lunch3);
-        cv_j_mealPlan_lunch4 = findViewById(R.id.cv_v_mealPlan_lunch4);
-        tv_j_mealPlan_lunch4 = findViewById(R.id.tv_v_mealPlan_lunch4);
-        cv_j_mealPlan_lunch5 = findViewById(R.id.cv_v_mealPlan_lunch5);
-        tv_j_mealPlan_lunch5 = findViewById(R.id.tv_v_mealPlan_lunch5);
+
         //DINNER
-        hsv_j_mealPlan_dinner = findViewById(R.id.hsv_v_mealPlan_dinner);
-        cv_j_mealPlan_dinner1 = findViewById(R.id.cv_v_mealPlan_dinner1);
-        tv_j_mealPlan_dinner1 = findViewById(R.id.tv_v_mealPlan_dinner1);
-        cv_j_mealPlan_dinner2 = findViewById(R.id.cv_v_mealPlan_dinner2);
-        tv_j_mealPlan_dinner2 = findViewById(R.id.tv_v_mealPlan_dinner2);
-        cv_j_mealPlan_dinner3 = findViewById(R.id.cv_v_mealPlan_dinner3);
-        tv_j_mealPlan_dinner3 = findViewById(R.id.tv_v_mealPlan_dinner3);
-        cv_j_mealPlan_dinner4 = findViewById(R.id.cv_v_mealPlan_dinner4);
-        tv_j_mealPlan_dinner4 = findViewById(R.id.tv_v_mealPlan_dinner4);
-        cv_j_mealPlan_dinner5 = findViewById(R.id.cv_v_mealPlan_dinner5);
-        tv_j_mealPlan_dinner5 = findViewById(R.id.tv_v_mealPlan_dinner5);
+
         //DATABASE
         dbHelper = new DatabaseHelper(this);
 
